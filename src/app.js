dedeff4032e6308678715172c6240a8df2b5365b
steven urbina <steven_urbina@hotmail.com>
2022-08-22 13:26:57 -0400

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,39 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//arrays:
+
+window.onload = () => {
+  document.querySelector("#listener").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = excuseGenerator();
+  });
+};
+
+let excuseGenerator = () => {
+  let who = [" a dog", " my granma", "a turtle", " a bird", " some nerd"];
+  let action = ["eat", "pissed", "crushed", "broked", "stole my"];
+  let things = ["my notes", "shoes", "heart", "dirty clothes"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  //random funtions
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let thingsIndex = Math.floor(Math.random() * things.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    things[thingsIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
